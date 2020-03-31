eacd0951c7294115b12e35b6ec2059f8e1a962e6
Vladimir Kotal <vlada@devnull.cz>
2020-03-31 20:00:34 +0200

    add identity

--- dev/push.sh
+++ dev/push.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 pwd
 cd repo
 ls -al
 date > date.txt
+git config --global user.email "vlada@devnull.cz"
+git config --global user.name "Vladimir Kotal"
 git commit -m "new date"
 git push
